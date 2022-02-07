283f9fc46258a73a9db96b19c573802000ec9715
Joshua Hamby <joshua.hamby@aggiemail.usu.edu>
2022-02-07 16:59:45 -0700

    test database for high level prototype

--- group2/lawndromat/models.py
+++ group2/lawndromat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userName = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    userTitle = models.TextChoices("Customer", "Worker")
+    password = models.CharField(max_length=50)
+    userPermission = models.CharField(max_length=10)
+    money = models.IntegerField()
+    
