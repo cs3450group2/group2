7048d15e8fb40fa588d2a45998156654dcda17b4
Joshua Hamby <joshua.hamby@aggiemail.usu.edu>
2022-02-22 12:45:49 -0700

    create new user based on register page update database

--- group2/lawndromat/views.py
+++ group2/lawndromat/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import redirect, render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import User, Request
+from django import forms
+import json
+import hashlib
+import os
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
+        salt = os.urandom(32)
+        hPasword = hashlib.pbkdf2_hmac('sha256',
+                                       request.POST["password"].encode("utf-8"),
+                                       salt,
+                                       100000,
+                                       dklen=128)
+        user = User.objects.create(userID = 1,
+                                        userZipCode = request.POST["zipcode"],
+                                        userName = request.POST["name"],
+                                        email = request.POST["email"],
+                                        userType = request.POST["type"],
+                                        password = hPasword,
+                                        money=0)
+        user.save()        
         return redirect('/', permanent=False)
     return render(request, "register.html")
 
