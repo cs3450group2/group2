f8747434329a494d9693e6390d0a10177e8f4077
Trevor Jex <jextrevor@gmail.com>
2022-02-22 11:10:11 -0700

    clear out address field

--- group2/lawndromat/static/js/register.js
+++ group2/lawndromat/static/js/register.js
@@ -1,5 +1,6 @@
 document.getElementById("worker").onchange = () => {
   document.getElementById("addressFormItem").style.display = "none";
+  document.getElementById("address").value = "";
 };
 document.getElementById("customer").onchange = () => {
   document.getElementById("addressFormItem").style.display = "block";
