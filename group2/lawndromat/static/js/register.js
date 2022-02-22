0bbbb30b87805fb8df3cce8eebf807dd015b6435
Trevor Jex <jextrevor@gmail.com>
2022-02-22 11:01:49 -0700

    hide address if you're a worker

---
+++ group2/lawndromat/static/js/register.js
@@ -0,0 +1,6 @@
+document.getElementById("worker").onchange = () => {
+  document.getElementById("addressFormItem").style.display = "none";
+};
+document.getElementById("customer").onchange = () => {
+  document.getElementById("addressFormItem").style.display = "block";
+};
