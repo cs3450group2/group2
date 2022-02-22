3d6a6d3c50d794b811cbad9adeb47580c0283209
Trevor Jex <jextrevor@gmail.com>
2022-02-21 19:41:38 -0700

    Add a starter form

--- group2/lawndromat/views.py
+++ group2/lawndromat/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 def register(request):
+    if request.method == "POST":
+        return redirect('/login', permanent=False)
     return render(request, "register.html")
