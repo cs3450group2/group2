ed41ddc3756b3ca8b27fe9555aa7b63144bf0429
Trevor Jex <jextrevor@gmail.com>
2022-03-24 11:44:50 -0600

    individual request page

--- group2/lawndromat/views.py
+++ group2/lawndromat/views.py
@@ -114,9 +114,29 @@ def openRequests(request):
 
 @login_required
 def request(request, id):
-    print(id)
-    #TODO: create individual page
-    return render(request, 'profile.html')
+    if request.user.userprofile.userType == "customer":
+        r = Request.objects.get(id=id, customerID=request.user.id)
+        if r.workerID is not None:
+            r.worker = User.objects.get(id=r.workerID)
+    elif request.user.userprofile.userType == "worker":
+        r = Request.objects.get(id=id)
+        if r.workerID is not None and r.workerID != request.user.id:
+            return redirect('/request/', permanent=False)
+        r.customer = User.objects.get(id=r.customerID)
+        r.cost = r.cost * 0.95
+    else:
+        r = Request.objects.get(id=id)
+    
+    if r.type == "lawn":
+        r.typeFormatted = "Lawn Mowing"
+    elif r.type == "rake":
+        r.typeFormatted = "Leaf Raking"
+    else:
+        r.typeFormatted = "Snow Shoveling"
+    
+    r.timeOfDayFormatted = r.timeOfDay.capitalize()
+
+    return render(request, 'request.html', {'request': r})
 @login_required
 def profile(request):
     return render(request, "profile.html")
